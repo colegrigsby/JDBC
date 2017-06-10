ec0fbc0760ad31ede45303bc77d25d4689785813
RAWeber <RAWeber95@gmail.com>
2017-06-10 16:07:48 -0700

    Added more general queries

--- general-queries.sql
+++ general-queries.sql
@@ -63,14 +63,15 @@ ORDER BY Vol
 ;
 
 -- Q3
-SELECT Y.Year, Y.Ticker
+SELECT Y.Year, Y.Ticker, S.Name
 FROM (SELECT Ticker, YEAR(Day) AS Year, MIN(Day) AS Min, MAX(Day) AS Max
     FROM Prices P
-    GROUP BY Ticker, YEAR(Day)) as Y, Prices P1, Prices P2
+    GROUP BY Ticker, YEAR(Day)) as Y, Prices P1, Prices P2, Securities S
 WHERE P1.Day = Y.Min
     AND P2.Day = Y.Max
     AND P1.Ticker = Y.Ticker
     AND P2.Ticker = Y.Ticker
+    AND S.Ticker = Y.Ticker
     AND 5 >= 
         (SELECT COUNT(*)
             FROM (SELECT Ticker, YEAR(Day) AS Year,
@@ -87,4 +88,46 @@ ORDER BY Y.Year, Y.Ticker;
 
 -- Q4
 
---Q5
+-- Q5.1
+SELECT Sector, SUM(up), SUM(down), SUM(up)/SUM(down)
+FROM (SELECT Sector,
+            IF(Close > Open, 1, 0) as Up, IF(Close > Open, 0, 1) as Down
+        FROM AdjustedPrices AP, Securities S
+        WHERE AP.Ticker = S.Ticker
+            AND YEAR(Day) = 2016
+            AND Sector != 'Telecommunications Services') X
+GROUP BY Sector
+;
+
+-- Q5.2
+SELECT Sector, SUM(AP2.Close) - SUM(AP1.Open)
+FROM Securities S,
+    (SELECT Ticker, Open
+    FROM AdjustedPrices A
+    WHERE Day = (SELECT MIN(DAY)
+                FROM AdjustedPrices
+                WHERE YEAR(Day) = 2016)) AP1,
+    (SELECT Ticker, Close
+    FROM AdjustedPrices A
+    WHERE Day = (SELECT MAX(DAY)
+                FROM AdjustedPrices
+                WHERE YEAR(Day) = 2016)) AP2
+WHERE AP1.Ticker = AP2.Ticker
+    AND S.Ticker = AP1.Ticker
+GROUP BY Sector
+;
+
+-- Q5.3
+SELECT Sector, SUM(Volume), (SUM(Volume) - A.Avg) / A.StDev as Stv
+    FROM (SELECT STDDEV(V) as StDev, AVG(V) as Avg
+            FROM (SELECT SUM(Volume) as V
+                    FROM AdjustedPrices AP, Securities S
+                    WHERE YEAR(Day) = 2016
+                        AND AP.Ticker = S.Ticker
+                    GROUP BY Sector) X
+            ) A, AdjustedPrices AP, Securities S
+    WHERE YEAR(Day) = 2016
+        AND AP.Ticker = S.Ticker
+    GROUP BY Sector
+;
+
